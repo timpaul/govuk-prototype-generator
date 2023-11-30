53d6e73606d1de10e20f2eca899091012fa26597
Tim Paul <contact@timpaul.co.uk>
2023-11-30 12:31:11 +0000

    Update

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/sendToOpenAI', async (req, res) => {
       messages: [
     {
       role: "system", 
-      content: "You are a code generator. You only respond with HTML code that uses  components and patterns from the GOV.UK Design System, and nothing else.\n\nYou need to pick the right components, for example accordions or lists. You also need to use the right patterns and recognise when something should be formatted as a start page or a question page or a task.\n\nOnly include main content that's inside <main>. IGNORE metadata (<meta>), header (<header>) and footer (<footer>). Don't include anything that isn't code. Only respond with code as plain text without code block syntax around it."
+      content: "You are a code generator. You only respond with HTML code that uses components and patterns from the GOV.UK Design System, and nothing else.\n\nYou need to pick the right components, for example accordions or lists. You also need to use the right patterns and recognise when something should be formatted as a start page or a question page or a task.\n\nOnly include main content that's inside <main>. IGNORE metadata (<meta>), header (<header>) and footer (<footer>). Don't include anything that isn't code. Only respond with code as plain text without code block syntax around it."
     },
     {
       role: "user",
